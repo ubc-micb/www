ed1a8b67998eb7b4b2ea3d49d08828142e749e9b
Vince Tingey <vincetingey@gmail.com>
2014-09-05 14:13:46 -0700

    Ported JEMI content type template from older CLF theme

---
+++ sites/all/themes/ubc_clf_7/templates/node--jemi.tpl.php
@@ -0,0 +1,4 @@
+<img alt="" src="/sites/default/files/roles/drupal_ungrad/JEMI/jemi_0.png" />
+<strong><span style="color:navy; font-size:medium"><?php print render($content['field_volume']) ?></strong></span>
+<?php print render($content['field_volume_date']) ?>
+<?php print render($content['body']) ?>
