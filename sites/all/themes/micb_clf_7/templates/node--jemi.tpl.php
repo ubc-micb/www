99cbcebe7a33f7bbeefb9b45f2f6564a3444097a
Vince Tingey <vincetingey@gmail.com>
2014-10-01 11:12:40 -0700

    Cleaned up JEMI template https://github.com/ubc-micb/www/issues/16

--- sites/all/themes/micb_clf_7/templates/node--jemi.tpl.php
+++ sites/all/themes/micb_clf_7/templates/node--jemi.tpl.php
@@ -1,4 +1,4 @@
-<img alt="" src="/sites/default/files/roles/drupal_ungrad/JEMI/jemi_0.png" />
-<strong><span style="color:navy; font-size:medium"><?php print render($content['field_volume']) ?></strong></span>
-<?php print render($content['field_volume_date']) ?>
-<?php print render($content['body']) ?>
+<img class="jemi-header" alt="JEMI" src="/sites/default/files/roles/drupal_ungrad/JEMI/jemi_0.png" />
+<div class="jemi-volume"><?php print render($content['field_volume']) ?></div>
+<div class="jemi-date"><?php print render($content['field_volume_date']) ?></div>
+<div class="jemi-body"><?php print render($content['body']) ?></div>
